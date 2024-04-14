ed5f3586ee9b973ca7061fb91f4f00bd1990a5f1
Sayan Mallick <snyxmk@gmail.com>
2024-04-14 13:15:29 +0530

    file stream

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod email_service;
 pub mod shutdown;
+pub mod uploader;
 pub mod util;
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,10 +3,10 @@ use serde::Deserialize;
 use std::{env, sync::Arc, time::Duration};
 
 use axum::{
-    extract::{DefaultBodyLimit, Multipart, State},
+    extract::{DefaultBodyLimit, Multipart, Path, Request, State},
     http::StatusCode,
     response::IntoResponse,
-    routing::post,
+    routing::{get, post},
     Form, Router,
 };
 
@@ -43,41 +43,44 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect_lazy("postgres://postgres:password@localhost:5432/devdump")
-        .unwrap();
-
-    // running slqx migration
-    sqlx::migrate!().run(&pool).await.unwrap();
-
-    // redis
-
-    let redis_client = Arc::new(redis::Client::open("redis://127.0.0.1/").unwrap());
-
+    // let pool = PgPoolOptions::new()
+    //     .max_connections(10)
+    //     .connect_lazy("postgres://postgres:password@localhost:5432/devdump")
+    //     .unwrap();
+    //
+    // // running slqx migration
+    // sqlx::migrate!().run(&pool).await.unwrap();
+    //
+    // // redis
+    //
+    // let redis_client = Arc::new(redis::Client::open("redis://127.0.0.1/").unwrap());
+    //
     tokio::fs::create_dir_all("./upload").await.unwrap();
-
-    let hostname = env::var("HOST").unwrap_or("http://localhost:3000".to_string());
-    let config = Config {
-        hostname,
-        pgpool: pool,
-        redis: redis_client,
-    };
+    //
+    // let hostname = env::var("HOST").unwrap_or("http://localhost:3000".to_string());
+    // let config = Config {
+    //     hostname,
+    //     pgpool: pool,
+    //     redis: redis_client,
+    // };
     // tracing::info!("{:?}", config);
 
     let app = Router::new()
         .route("/", post(upload))
-        .route("/signup", post(signup))
+        .route("/file/:file_name", post(stream))
+        .route("/health_check", get(health_check))
+        //     .route("/signup", post(signup))
         .layer(DefaultBodyLimit::disable())
         .layer(RequestBodyLimitLayer::new(250 * 1024 * 1024)) // 250MB
         .nest_service("/d", ServeDir::new("upload"))
         .layer((
             TraceLayer::new_for_http(),
             TimeoutLayer::new(Duration::from_secs(5)),
-        ))
-        .with_state(config);
+        ));
+    // .with_state(config);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     tracing::info!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app)
@@ -86,37 +89,42 @@ async fn main() {
         .unwrap();
 }
 
-async fn signup(
-    State(config): State<Config>,
-    Form(login_form): Form<LoginForm>,
-) -> impl IntoResponse {
-    tracing::info!("signup: {:?}", login_form);
-
-    match sqlx::query!(
-    "INSERT INTO users (id, email, created_on, last_login, status) VALUES ($1, $2, $3, NULL, 'pending_confirmaton')",
-    uuid::Uuid::new_v4(),
-    login_form.email,
-    chrono::Utc::now()
-    ).execute(&config.pgpool).await {
-        Ok(_) => {
-            let token = uuid::Uuid::new_v4().to_string();
-            let mut conn = config.redis.get_async_connection().await.unwrap();
-            conn.set_ex::<String, String, ()>(token.to_owned(), login_form.email.to_owned(), 60 * 30).await.unwrap();
-            let body = format!(
-                "Click this link to confirm your email: {}/confirm?token={}",
-                config.hostname, token
-            );
-            send_email(login_form.email, "Confirm your email".to_string(), body).await;
-            return (StatusCode::OK, "ok");
-        }
-        Err(e) => {
-            tracing::error!("error: {}", e);
-            return (StatusCode::INTERNAL_SERVER_ERROR, "error");
-        }
-    };
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
-async fn upload(State(config): State<Config>, mut file: Multipart) -> String {
+// async fn signup(
+//     State(config): State<Config>,
+//     Form(login_form): Form<LoginForm>,
+// ) -> impl IntoResponse {
+//     tracing::info!("signup: {:?}", login_form);
+//
+//     match sqlx::query!(
+//     "INSERT INTO users (id, email, created_on, last_login, status) VALUES ($1, $2, $3, NULL, 'pending_confirmaton')",
+//     uuid::Uuid::new_v4(),
+//     login_form.email,
+//     chrono::Utc::now()
+//     ).execute(&config.pgpool).await {
+//         Ok(_) => {
+//             let token = uuid::Uuid::new_v4().to_string();
+//             let mut conn = config.redis.get_async_connection().await.unwrap();
+//             conn.set_ex::<String, String, ()>(token.to_owned(), login_form.email.to_owned(), 60 * 30).await.unwrap();
+//             let body = format!(
+//                 "Click this link to confirm your email: {}/confirm?token={}",
+//                 config.hostname, token
+//             );
+//             send_email(login_form.email, "Confirm your email".to_string(), body).await;
+//             return (StatusCode::OK, "ok");
+//         }
+//         Err(e) => {
+//             tracing::error!("error: {}", e);
+//             return (StatusCode::INTERNAL_SERVER_ERROR, "error");
+//         }
+//     };
+// }
+
+// State(config): State<Config>,
+async fn upload(mut file: Multipart) -> String {
     let mut d = String::new();
     while let Some(f) = file.next_field().await.unwrap() {
         let id = generate_id(6);
@@ -138,7 +146,16 @@ async fn upload(State(config): State<Config>, mut file: Multipart) -> String {
         file.write_all(&data).await.unwrap();
 
         tracing::info!("upload file: {}", file_name);
-        d = format!("{}/d/{}", config.hostname, file_name); // get from env
+        d = format!("{}/d/{}", "localhost:3000", file_name); // get from env
     }
-    d
+    return d;
+}
+
+// stream to file
+async fn stream(
+    Path(file_name): Path<String>,
+    request: Request,
+) -> Result<(), (StatusCode, String)> {
+    return server::uploader::stream_to_file(&file_name, request.into_body().into_data_stream())
+        .await;
 }
